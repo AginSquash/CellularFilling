b1ee65729702e21e96d223936cad8bffcc1e922b
AginS <agins.main@gmail.com>
2024-04-05 18:52:34 +0300

    Initial Commit

---
+++ CellularFilling/CellularFillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularFillingApp.swift
+//  CellularFilling
+//
+//  Created by Vlad Vrublevsky on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CellularFillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
